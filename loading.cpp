4fb8f64cc9ef14518ded200c8e4b84595623798e
Arbin Chhatkuli <94787971+Starrpix@users.noreply.github.com>
2023-03-30 14:41:45 +0545

    Create loading.cpp

---
+++ loading.cpp
@@ -0,0 +1,21 @@
+/*
+Loading...
+It’s very common to have a "preloader" component, especially in multifunctional apps and websites.
+
+Task
+Complete the given function to output "Loading...", then call it.
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+void loading(){
+    cout << "Loading...";
+}
+
+
+int main() {
+    loading(); 
+}
