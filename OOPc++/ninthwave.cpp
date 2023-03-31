d5746259214224002d12b6774609bf8a25398168
Arbin Chhatkuli <94787971+Starrpix@users.noreply.github.com>
2023-03-31 21:37:46 +0545

    Create ninthwave.cpp

---
+++ OOPc++/ninthwave.cpp
@@ -0,0 +1,16 @@
+/*Constructors 
+The given program defines a Painting class and creates an object using the constructor.
+
+Fix the code so that the constructor takes the name as the argument and outputs it.
+
+Sample Input
+
+Ocean
+
+Sample Output
+
+Ocean
+*/
+
+
+
