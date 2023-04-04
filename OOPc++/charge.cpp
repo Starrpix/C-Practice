a804cf5cbcd105f1bf9cc1ba1f4e2ee31b81ce9d
Arbin Chhatkuli <94787971+Starrpix@users.noreply.github.com>
2023-04-04 21:26:17 +0545

    Create charge.cpp

---
+++ OOPc++/charge.cpp
@@ -0,0 +1,39 @@
+/*
+
+Selection Operator 
+You are creating a program to manage smartphones. The given code declares a
+ Phone class, with its constructor and two methods: use() and getCharge().
+
+A Phone object is declared in main. Complete the code to call the getCharge()
+method using the correct selection operator.
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+class Phone
+{
+    public:
+        int charge;
+        Phone() {
+            charge = 100;
+        }
+        void use() {
+            charge -=10;
+        }
+        void getCharge() {
+            cout << charge;
+        }
+};
+
+int main() {
+    Phone p;
+    p.use();
+    Phone *ptr = &p;
+    ptr->getCharge();
+    
+    //call the getCharge() method on ptr
+    
+} 
